bc4924c70e3b5276179abbc6c6cb024e8e7dc2de
HGB <hsw8147@gmail.com>
2023-07-06 10:27:35 +0900

    aws setting 추가

--- config/settings.py
+++ config/settings.py
@@ -189,8 +189,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # AWS Access
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_DEFAULT_ACL = "public-read"
 AWS_REGION = os.getenv("AWS_REGION")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
 # Static Setting
 AWS_LOCATION = "static"
