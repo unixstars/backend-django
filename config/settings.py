ccb83124e24dd0d25073cbb28ad5526cdb9e0565
HGB <hsw8147@gmail.com>
2023-07-06 11:55:07 +0900

    static nginx 설정

--- config/settings.py
+++ config/settings.py
@@ -196,9 +196,6 @@ AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 # Static Setting
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    STATIC_ROOT,
-]
 
 
 # Media Setting
