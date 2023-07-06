3eaa5804a9e0f67d651360769f10b7c15fae979d
HGB <hsw8147@gmail.com>
2023-07-06 10:44:06 +0900

    aws setting 변경

--- config/settings.py
+++ config/settings.py
@@ -202,6 +202,12 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
+
 
 # Media Setting
 MEDIA_LOCATION = "media"
