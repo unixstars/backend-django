b5f106fa181a54a84a40c31322f1a71805ffb2d1
HGB <hsw8147@gmail.com>
2023-07-11 14:37:48 +0900

    chore: default db dev setting change

--- config/settings.py
+++ config/settings.py
@@ -154,19 +154,17 @@ DATABASES = {
 
 
 DATABASES = {
-    """
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": "3306",
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    },
-    """
+    # "default": {
+    #    "ENGINE": "django.db.backends.mysql",
+    #    "NAME": os.getenv("DB_NAME"),
+    #    "USER": os.getenv("DB_USER"),
+    #    "PASSWORD": os.getenv("DB_PASSWORD"),
+    #    "HOST": os.getenv("DB_HOST"),
+    #    "PORT": "3306",
+    #    "OPTIONS": {
+    #        "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+    #    },
+    # },
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DB_NAME_DEV"),
