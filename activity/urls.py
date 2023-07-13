83a8874c8265c413171d744fb752ae4088d300f9
HGB <hsw8147@gmail.com>
2023-07-13 16:29:22 +0900

    feat: company user register

--- activity/urls.py
+++ activity/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BoardListView
 
 urlpatterns = [
-    path("board/list/", BoardListView.as_view(), name="board_list"),
+    path("board/list/", BoardListView.as_view(), name="board-list"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,91 +1,206 @@
-"""
-from user.models import User
-from .serializers import (
-    UserAuthSerializer,
-    GeneralUserAuthSerializer,
-    CompanyUserAuthSerializer,
-    RefreshTokenSerializer,
-)
-from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
+from rest_framework import views
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.exceptions import TokenError
+from django.core.cache import cache
+import requests, os, random
+from .serializers import (
+    CompanyVerificationSerializer,
+    CompanyManagerEmailVerificationSerializer,
+    CompanyManagerPhoneVerificationSerializer,
+    CompanyUserRegistrationSerializer,
+)
+from .ncloud import get_api_keys
+from dj_rest_auth.registration.views import RegisterView
 
 
-class GeneralRegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = GeneralUserAuthSerializer
-    permission_classes = (AllowAny,)
+class CompanyVerificationView(views.APIView):
+    serializer_class = CompanyVerificationSerializer
 
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-class CompanyRegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = CompanyUserAuthSerializer
-    permission_classes = (AllowAny,)
-
-
-class LoginView(generics.GenericAPIView):
-    serializer_class = UserAuthSerializer
-    permission_classes = (AllowAny,)
-
-    # JWT 토큰을 사용하는 로그인 로직
-    def post(self, request, *args, **kwargs):
-        email = request.data.get("email")
-        password = request.data.get("password")
-        user = authenticate(request, email=email, password=password)
-
-        if user is not None:
-            # 로그인 로직 구현 - JWT 토큰 사용
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-
-            return Response(
-                {
-                    "access_token": access_token,
-                    "refresh_token": str(refresh),
+        # 국세청 사업자등록 진위여부 API 호출
+        try:
+            service_key = os.getenv("BUSINESS_SERVICE_KEY")
+            response = requests.post(
+                f"https://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey={service_key}",
+                json={
+                    "businesses": [
+                        {
+                            "b_no": serializer.validated_data.get("business_number"),
+                            "start_dt": serializer.validated_data.get(
+                                "start_date"
+                            ).strftime(
+                                "%Y%m%d"
+                            ),  # Date를 string으로 변환
+                            "p_nm": serializer.validated_data.get("ceo_name"),
+                            "corp_no": serializer.validated_data.get(
+                                "corporate_number"
+                            ),
+                        }
+                    ]
                 },
-                status=status.HTTP_200_OK,
             )
+            response.raise_for_status()
+            # 응답코드가 4xx,5xx 인 경우 HTTPError 발생시키기
+        except requests.exceptions.HTTPError:
+            if response.status_code == 400:
+                return Response({"detail": "JSON 포맷에 맞지 않는 데이터입니다."}, status=400)
+            elif response.status_code == 404:
+                return Response({"detail": "서비스를 찾을 수 없습니다."}, status=404)
+            elif response.status_code == 411:
+                return Response({"detail": "필수 요청 파라미터를 누락하였습니다."}, status=400)
+            elif response.status_code == 413:
+                return Response(
+                    {"detail": "요청 기업의 수가 100개 이상입니다."},
+                    status=400,
+                )
+            elif response.status_code == 500:
+                return Response({"detail": "내부 서버 오류입니다."}, status=500)
+            else:
+                return Response({"detail": "알 수 없는 오류가 발생했습니다."}, status=500)
+
+        data = response.json()
+        if not data.get("status_code") == "OK":
+            return Response({"detail": "기업 정보가 유효하지 않습니다."}, status=400)
+        return Response(data)
+
+
+class CompanyManagerPhoneSendView(views.APIView):
+    def post(self, request):
+        manager_phone = request.data.get("manager_phone")
+        # 랜덤난수 생성
+        manager_phone_auth_number = random.randint(100000, 999999)
+        # 캐시 5분 저장
+        cache.set(manager_phone, manager_phone_auth_number, 60 * 5)
+
+        # 네이버 클라우드 SMS API
+        try:
+            service_id = os.getenv("SMS_SERVICE_ID")
+            uri = f"/sms/v2/services/{service_id}/messages"
+            timestamp, access_key, signing_key = get_api_keys(uri, "POST")
+
+            headers = {
+                "Content-Type": "application/json; charset=utf-8",
+                "x-ncp-apigw-timestamp": timestamp,
+                "x-ncp-iam-access-key": access_key,
+                "x-ncp-apigw-signature-v2": signing_key,
+            }
+
+            data = {
+                "type": "SMS",
+                "contentType": "COMM",
+                "from": os.getenv("SMS_CALLING_NUM"),
+                "subject": "[유니스타 기업회원가입 담당자 인증]",
+                "content": f"인증번호는 [{manager_phone_auth_number}]입니다. 인증번호를 정확히 입력해주세요.",
+                "messages": [{"to": f"{manager_phone}"}],
+            }
+
+            response = requests.post(
+                f"https://sens.apigw.ntruss.com/{uri}",
+                headers=headers,
+                json=data,
+            )
+            response.raise_for_status()
+
+        except requests.exceptions.HTTPError:
+            if response.status_code != 202:
+                return Response(
+                    {"detail": "메시지 전송에 실패했습니다."}, status=response.status_code
+                )
+
+        return Response(response.json())
+
+
+class CompanyManagerPhoneVerificationView(views.APIView):
+    serializer_class = CompanyManagerPhoneVerificationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        manager_phone = serializer.validated_data.get("manager_phone")
+        auth_number = serializer.validated_data.get("auth_number")
+
+        correct_auth_number = cache.get(manager_phone)
+
+        if correct_auth_number is None:
+            return Response({"detail": "인증번호가 만료되었습니다."}, status=400)
+        elif correct_auth_number != auth_number:
+            return Response({"detail": "잘못된 인증번호 입니다."}, status=400)
         else:
-            return Response({"detail": "로그인 실패!"}, status=status.HTTP_401_UNAUTHORIZED)
+            # 인증여부 캐시에 추가(최종 회원가입에 활용)
+            cache.set(manager_phone + "_authenticated", True, 60 * 60)
+            return Response({"detail": "인증에 성공하였습니다."})
 
 
-class TokenRefreshView(generics.GenericAPIView):
-    serializer_class = RefreshTokenSerializer
-    permission_classes = (AllowAny,)
-
-    def post(self, request, *args, **kwargs):
-        refresh_token = request.data.get("refresh_token")
-
-        if refresh_token is None:
-            return Response(
-                {"error": "Refresh token이 필요합니다."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+class CompanyManagerEmailSendView(views.APIView):
+    def post(self, request):
+        manager_email = request.data.get("manager_email")
+        manager_email_auth_code = random.randint(100000, 999999)
+        cache.set(manager_email, manager_email_auth_code, 60 * 5)
 
         try:
-            token = RefreshToken(refresh_token)
-            access_token = str(token.access_token)
+            uri = "/api/v1/mails"
+            timestamp, access_key, signing_key = get_api_keys(uri, "POST")
 
-            return Response(
-                {
-                    "access_token": access_token,
-                },
-                status=status.HTTP_200_OK,
+            headers = {
+                "Content-Type": "application/json; charset=utf-8",
+                "x-ncp-apigw-timestamp": timestamp,
+                "x-ncp-iam-access-key": access_key,
+                "x-ncp-apigw-signature-v2": signing_key,
+            }
+
+            data = {
+                "senderAddress": os.getenv("EMAIL_SENDER_ADDRESS"),
+                "title": "유니스타 기업회원가입 인증 메일",
+                "body": f"유니스타 기업회원 메일 인증 코드는 {manager_email_auth_code}입니다. 인증코드를 정확히 입력해 주세요.",
+                "recipients": [
+                    {
+                        "address": f"{manager_email}",
+                        "type": "R",
+                    }
+                ],
+                "individual": True,
+                "advertising": False,
+            }
+
+            response = requests.post(
+                f"https://mail.apigw.ntruss.com/{uri}",
+                headers=headers,
+                json=data,
             )
-        except TokenError:
-            return Response(
-                {"error": "잘못된 Refresh token입니다."}, status=status.HTTP_401_UNAUTHORIZED
-            )
-"""
-from user.models import CompanyUser
-from .serializers import CompanyUserSerializer
-from rest_framework import generics
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            if response.status_code != 201:
+                return Response(
+                    {"detail": "이메일 전송에 실패했습니다."}, status=response.status_code
+                )
+
+        return Response(response.json())
 
 
-# Test
-class CompanyUserView(generics.GenericAPIView):
-    queryset = CompanyUser.objects.all()
-    serializer_class = CompanyUserSerializer
+class CompanyManagerEmailVerificationView(views.APIView):
+    serializer_class = CompanyManagerEmailVerificationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        manager_email = serializer.validated_data.get("manager_email")
+        auth_code = serializer.validated_data.get("auth_code")
+
+        correct_auth_code = cache.get(manager_email)
+
+        if correct_auth_code is None:
+            return Response({"detail": "인증코드가 만료되었습니다."}, status=400)
+        elif correct_auth_code != auth_code:
+            return Response({"detail": "잘못된 인증코드 입니다."}, status=400)
+        else:
+            # 인증여부 캐시에 추가(최종 회원가입에 활용)
+            cache.set(manager_email + "_authenticated", True, 60 * 60)
+            return Response({"detail": "인증에 성공하였습니다."})
+
+
+class CompanyUserRegisterView(RegisterView):
+    serializer_class = CompanyUserRegistrationSerializer
