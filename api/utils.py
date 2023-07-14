acceac9f7f2f5642a3d6d07e102333f0cdec00ad
HGB <hsw8147@gmail.com>
2023-07-15 01:48:28 +0900

    refactor: 항목 기준으로 send view 횟수 제한, throttle use

--- api/utils.py
+++ api/utils.py
@@ -2,6 +2,7 @@ import boto3
 from django.conf import settings
 from botocore.exceptions import NoCredentialsError
 import hashlib
+from rest_framework.throttling import SimpleRateThrottle
 
 
 # media 파일의 주소에 대해 사인된 url을 생성하는 함수
@@ -24,10 +25,24 @@ def generate_presigned_url(bucket_name, object_name, expiration=300):
 
 
 # hash 함수, one way
-
-
 def hash_function(data):
     m = hashlib.sha256()
     m.update(data.encode("utf-8"))
     result = m.hexdigest()
     return result
+
+
+# request의 특정항목에 대한 제한
+class FieldRateThrottle(SimpleRateThrottle):
+    rate = "10/hour"
+    field = None
+
+    def get_cache_key(self, request, view):
+        self.field = getattr(view, "throttle_field", None)
+        if self.field and self.field in request.data:
+            return self.cache_format % {
+                "scope": self.scope,
+                "ident": request.data[self.field],
+            }
+
+        return None
