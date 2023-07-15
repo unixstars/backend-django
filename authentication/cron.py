fff4fa2daab7d025177a08d4b61f9e20ff85756f
HGB <hsw8147@gmail.com>
2023-07-15 23:47:53 +0900

    refactor: authentication.cron

--- authentication/cron.py
+++ authentication/cron.py
@@ -1,10 +1,6 @@
 from django.utils.timezone import now
-from rest_framework_simplejwt.token_blacklist.models import (
-    OutstandingToken,
-    BlacklistedToken,
-)
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
 
 
 def delete_expired_tokens():
     OutstandingToken.objects.filter(expires_at__lt=now()).delete()
-    BlacklistedToken.objects.filter(token__expires_at__lt=now()).delete()
