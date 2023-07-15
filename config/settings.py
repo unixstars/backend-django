5c761cd2ac5e8bceea1e4ad3b50702c66a3aec84
HGB <hsw8147@gmail.com>
2023-07-15 19:07:38 +0900

    refactor: time config change

--- config/settings.py
+++ config/settings.py
@@ -233,7 +233,7 @@ TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static 파일들 (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
