fcda27ef471d04739f09f69c1a7e128be9a07f8d
HGB <hsw8147@gmail.com>
2023-07-15 23:29:50 +0900

    refactor: Login Event import User

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class LoginEvent(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     ip_address = models.GenericIPAddressField()
