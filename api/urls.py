fbae0e5ad512c5623bc4c169e3bda8ba79a5c35f
HGB <hsw8147@gmail.com>
2023-07-19 03:12:10 +0900

    refactor: authentication login url 앞쪽 배치

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("activity.urls")),
+    path("auth/", include("authentication.urls")),
     path("auth/", include("dj_rest_auth.urls")),
     path("auth/", include("dj_rest_auth.registration.urls")),
-    path("auth/", include("authentication.urls")),
 ]
