ac6ec437cec5081218a1400d4738406e0d06d23a
HGB <hsw8147@gmail.com>
2023-07-18 17:39:35 +0900

    refactor: allauth url remove

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path("", include("activity.urls")),
     path("auth/", include("dj_rest_auth.urls")),
     path("auth/", include("dj_rest_auth.registration.urls")),
-    path("accounts/", include("allauth.urls")),
     path("auth/", include("authentication.urls")),
 ]
