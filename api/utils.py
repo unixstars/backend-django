d96eed0ad754a665f759f3122ca9848a9fea20c6
HGB <hsw8147@gmail.com>
2023-07-24 21:48:29 +0900

    refactor: presigned_url 함수 수정

--- api/utils.py
+++ api/utils.py
@@ -11,6 +11,7 @@ def generate_presigned_url(bucket_name, object_name, expiration=300):
         "s3",
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_REGION,
     )
     try:
         response = s3_client.generate_presigned_url(
