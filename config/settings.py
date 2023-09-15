83ed4729b136ea361ee21974bc29de9f7e68e8e1
HGB <hsw8147@gmail.com>
2023-09-15 17:41:29 +0900

    production

--- config/settings.py
+++ config/settings.py
@@ -200,28 +200,28 @@ DATABASES = {
 
 
 DATABASES = {
-    # "default": {
-    #    "ENGINE": "django.db.backends.mysql",
-    #    "NAME": os.getenv("DB_NAME"),
-    #    "USER": os.getenv("DB_USER"),
-    #    "PASSWORD": os.getenv("DB_PASSWORD"),
-    #    "HOST": os.getenv("DB_HOST"),
-    #    "PORT": "3306",
-    #    "OPTIONS": {
-    #        "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-    #    },
-    # },
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME_DEV"),
-        "USER": os.getenv("DB_USER_DEV"),
-        "PASSWORD": os.getenv("DB_PASSWORD_DEV"),
-        "HOST": os.getenv("DB_HOST_DEV"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
         "PORT": "3306",
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     },
+    # "default": {
+    #    "ENGINE": "django.db.backends.mysql",
+    #    "NAME": os.getenv("DB_NAME_DEV"),
+    #    "USER": os.getenv("DB_USER_DEV"),
+    #    "PASSWORD": os.getenv("DB_PASSWORD_DEV"),
+    #    "HOST": os.getenv("DB_HOST_DEV"),
+    #    "PORT": "3306",
+    #    "OPTIONS": {
+    #        "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+    #    },
+    # },
 }
 
 
