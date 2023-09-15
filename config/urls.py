d93e3b685f14527138b9a49e2076b7949faf3720
HGB <hsw8147@gmail.com>
2023-09-15 23:51:46 +0900

    allowed hosts / 추가3

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("/", views.health_check()),
+    path("", views.health_check),
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")),
 ]
