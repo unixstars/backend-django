85a7b61a775358d3fc8f91078501658df78db242
HGB <hsw8147@gmail.com>
2023-10-06 22:21:28 +0900

    refactor: IsStaffOrReadOnlyPermission 수정

--- api/permissions.py
+++ api/permissions.py
@@ -73,6 +73,6 @@ class IsSubmitOwnerStudent(permissions.BasePermission):
 
 class IsStaffOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method == "GET" and request.user.is_authenticated:
+        if request.method == "GET":
             return True
-        return request.user.is_staff
+        return request.user.is_authenticated and request.user.is_staff
