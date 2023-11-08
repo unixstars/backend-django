9c8d3d65c2703074532f8915e08a793912df7ca4
HGB <hsw8147@gmail.com>
2023-11-08 10:48:09 +0900

    refactor: program detail total week 수정

--- program/serializers.py
+++ program/serializers.py
@@ -119,8 +119,8 @@ class ProgramDetailSerializer(serializers.ModelSerializer):
         return company_name
 
     def get_total_week(self, obj):
-        duration = obj.form.activity.board.duration
-        weeks, remaining_days = divmod(duration.days, 7)
+        period = obj.form.activity.period
+        weeks, remaining_days = divmod(period.days, 7)
         if remaining_days > 0:
             weeks += 1
         return weeks
