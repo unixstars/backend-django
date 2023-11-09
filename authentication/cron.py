088899b3ea03d6a8440f680c2ed76e22b67fccd0
HGB <hsw8147@gmail.com>
2023-11-09 21:39:46 +0900

    feat: cloudfront,cron log

--- authentication/cron.py
+++ authentication/cron.py
@@ -1,6 +1,13 @@
-from django.utils.timezone import now
+from django.utils import timezone
 from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
+import logging
+
+# 로거 설정
+logger = logging.getLogger("cron_info")
 
 
 def delete_expired_tokens():
-    OutstandingToken.objects.filter(expires_at__lt=now()).delete()
+    now = timezone.now()
+    count, _ = OutstandingToken.objects.filter(expires_at__lt=now).delete()
+    logger.info("Delete %d expired tokens at %s", count, now)
+    logger.info("EXPIRED OUTSTANDING TOKEN DELETE COMPLETED %s", now)
