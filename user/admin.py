6b2f378a891608178846d7b1c77eba9eee502cc1
HGB <hsw8147@gmail.com>
2023-12-04 22:29:48 +0900

    refactor: admin 수정

--- user/admin.py
+++ user/admin.py
@@ -17,7 +17,7 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(StudentUser)
 class StudentUserAdmin(admin.ModelAdmin):
-    list_display = ["user"]
+    list_display = ["user", "student_user_profile.name"]
     search_fields = ["user"]
 
 
