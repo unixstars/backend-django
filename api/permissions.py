c63e3b86b4f7f9d2971df12aa422e6a03fc1c91a
HGB <hsw8147@gmail.com>
2023-12-07 19:47:43 +0900

    feat: super-login

--- api/permissions.py
+++ api/permissions.py
@@ -76,3 +76,8 @@ class IsStaffOrReadOnly(permissions.BasePermission):
         if request.method == "GET":
             return True
         return request.user.is_authenticated and request.user.is_staff
+
+
+class IsStaff(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_staff
--- api/views.py
+++ api/views.py
@@ -3,8 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import AppConfiguration
+from user.models import User
 from .serializers import AppConfigurationSerializer
-from .permissions import IsStaffOrReadOnly
+from .permissions import IsStaffOrReadOnly, IsStaff
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class AppConfigurationView(APIView):
@@ -30,3 +32,17 @@ class AppConfigurationView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetTokenForStaffView(APIView):
+    permission_classes = [IsStaff]  # 강력한 접근 제한
+
+    def get(self, request, email, format=None):
+        try:
+            user = User.objects.get(email=email)
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {"access": str(refresh.access_token), "refresh": str(refresh)}
+            )
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=404)
